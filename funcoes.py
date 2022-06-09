f17ae5dcdbeb3a00b59b686fcf589bd5d21a094f
TaYkEnN <107156787+TaYkEnN@users.noreply.github.com>
2022-06-08 22:48:26 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -3,7 +3,6 @@ import os
 def reiniciar_tela ():
     os.system ("cls")
 
-
 def menu_inicial():
     print('''
                  ╔╗ ╔═══╗ ╔═══╗ ╔═══╗    ╔═══╗ ╔═══╗     ╔═══╗ ╔═══╗ ╔═══╗ ╔═══╗ ╔═══╗
@@ -13,9 +12,9 @@ def menu_inicial():
                ║╚╝║ ║╚═╝║ ║╚╩═║ ║╚═╝║    ╔╝╚╝║ ║╔═╗║    ╔╝╚╗   ║╚═╝║ ║║║╚╗ ║╚═╝║ ║╔═╗║
                ╚══╝ ╚═══╝ ╚═══╝ ╚═══╝    ╚═══╝ ╚╝ ╚╝    ╚══╝   ╚═══╝ ╚╝╚═╝ ╚═══╝ ╚╝ ╚╝
 
-                             +====================================+
-                            || DEVELOPERS: TAINAN, VITOR, JUNIOR  ||
-                             +====================================+ 
+                              +====================================+
+                             || DEVELOPERS: TAINAN, VITOR, JUNIOR  ||
+                              +====================================+ 
 
 Obs: Por gentileza, não use a palavra 'PARALELEPíPEDO',ta muito manjada já.
 Tente não dar dicas logícas, exemplo a palavra escondida é "CAMA", pelo amor de Deus,\nnão vá por de dica 'onde a pessoa dorme', larga de ser burro!\n
